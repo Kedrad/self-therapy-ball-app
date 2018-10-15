81ad3432bc7dc189444993f7a6a98e48d3161bcf
kedrad <rkop@protonmail.com>
2018-10-15 19:44:26 +0200

    Fixed problem with vector graphic causing crash on the older devices

--- app/src/main/java/com/kedrad/selftherapyball/ExercisePlanActivity.java
+++ app/src/main/java/com/kedrad/selftherapyball/ExercisePlanActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -38,6 +39,7 @@ public class ExercisePlanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_plan);
         ButterKnife.bind(this);
